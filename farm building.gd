ebb2f731eb86399e0253ce694bccf474a8a5fd0c
fran streicher <fstreicher@fordham.edu>
2024-03-14 15:26:29 -0400

    godot

--- farm building.gd
+++ farm building.gd
@@ -1,1 +1,16 @@
 extends Sprite2D
+ 
+var pink_chicken_in_farm = false
+var blue_chicken_in_farm = false
+
+
+
+func _on_area_2d_body_entered(body):
+	print (body)
+	if body.name == "pinkchicken" :
+		pink_chicken_in_farm = true
+	if body.name == "bluechicken" :
+		blue_chicken_in_farm = true
+	if blue_chicken_in_farm and pink_chicken_in_farm :
+		print ("you win")
+		get_tree().change_scene_to_file("res://victory_screen.tscn")
